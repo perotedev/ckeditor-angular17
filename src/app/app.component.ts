a96babe6aef51e22e39946165aff9472c74f7bb3
Rodrigo Perote <rodripema@hotmail.com>
2024-03-08 00:48:14 -0400

    custom ckeditor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,51 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {ChangeEvent, CKEditorModule} from '@ckeditor/ckeditor5-angular';
+import CKSource from 'custom-ckeditor5/build/ckeditor';
+import {CommonModule} from "@angular/common";
+import {DomSanitizer} from "@angular/platform-browser";
+
+const Context = CKSource.Editor.Context;
+const Editor = CKSource.Editor;
+const ContextWatchdog = CKSource.Editor.ContextWatchdog;
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    CKEditorModule,
+    CommonModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'ckeditor';
+export class AppComponent implements OnInit{
+  @ViewChild("myEditor", { static: false }) myEditor: any;
+
+  text: string = "";
+  ckeditor: any = Editor;
+  watchdog: any;
+  ready = false;
+
+  constructor(
+    private sanitizer: DomSanitizer
+  ) { }
+
+  ngOnInit() {
+    const contextConfig = {};
+    this.watchdog = new ContextWatchdog( Context );
+
+    this.watchdog.create( contextConfig )
+      .then( () => {
+        this.ready = true;
+        console.log({ready: this.ready})
+      } );
+  }
+
+  getSafetyHtml(value: string) {
+    return this.sanitizer.bypassSecurityTrustHtml(value);
+  }
+
+  onChange({ editor }: ChangeEvent) {
+    this.text = editor.getData();
+  }
 }
